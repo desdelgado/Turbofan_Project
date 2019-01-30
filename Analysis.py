8ed6f2896a9de2be5cf397ce4b5c052874ce8c64
Kazi Sadman <kazisadman2014@u.northwestern.edu>
2019-01-30 16:50:20 -0600

    First working version

--- Analysis.py
+++ Analysis.py
@@ -7,7 +7,12 @@ Created on Thu Jan 31 02:04:58 2019
 
 import pandas as pd
 import matplotlib.pyplot as plt
-# import seaborn as sns; sns.set()
+import numpy as np
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import math
 
 feat_names = ['Unit', 'CycleNo', 'opset1', 'opset2', 'opset3', 'sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5',
               'sensor6', 'sensor7', 'sensor8', 'sensor9', 'sensor10', 'sensor11', 'sensor12', 'sensor13', 'sensor14',
@@ -18,32 +23,35 @@ data_train1 = pd.read_csv('./CMAPSSData/train_FD001.txt', sep = ' ', header = No
 # data_test1 = pd.read_csv('./CMAPSSData/test_FD001.txt', sep = ' ', header = None)
 # data_RUL1 = pd.read_csv('./CMAPSSData/RUL_FD001.txt', sep = ' ', header = None)
 
+# data_train1.isna().sum() # Finding NaNs
+
 # print(data_train1['Unit'].value_counts())
 
 # plt.plot(data_train1['Unit'].value_counts()) # no of data points per unit
 # plt.show()
 
-max_cycle = data_train1.groupby('Unit')['CycleNo'].max().reset_index()
+data_train1['RUL'] = ""
 
-# print(max_cycle.shape)
+max_cycle = data_train1.groupby('Unit')['CycleNo'].max().reset_index()
+# print(max_cycle)
+# print(max_cycle.loc[[1]])
+
+
 # Add checking for NaNs in all columns
 
-# data_train1['RUL'] =
+# print(max_cycle.iloc[2, 1])
 
-# for x in max_cycle[0]:
+# df.loc[df["Sex"] == "female", "Sex"] = 1
 
-# for x in max_cycle[0]:
-#     for k in data_train1['Unit']:
-#         if x == data_train1['Unit']:
+for x in max_cycle.iloc[:, 0]:
+    count_cycle = max_cycle.iloc[x-1, 1]
+    data_train1.loc[data_train1['Unit'] == x, 'RUL'] = count_cycle - data_train1['CycleNo']
 
+df = data_train1.drop(['RUL','sensor22','sensor23','sensor25','sensor26','sensor24'], axis=1)
 
-# unit_1 = data_train1[data_train1[0] == 1]
-
-# EDA of what each sensor or setting looks like
-# plt.close('all')
-#plot all the sensor data
-# plt.plot(unit_1[1], unit_1.loc[:,5:28], 'o' )
-# plt.show()
-#seems like theres quite the spread
-
-#%% Scale the sensor data in a meaningful way 
+X_train, X_test, y_train, y_test = train_test_split(df, data_train1["RUL"], test_size=0.7, random_state=5)
+leg = linear_model.LinearRegression()
+leg.fit(X_train, y_train)
+predictions = leg.predict(X_test)
+mse = mean_squared_error(y_test, predictions)
+print('RMSE = {}'.format(round(mse, 3)))
