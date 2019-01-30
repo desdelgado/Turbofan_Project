3675aa99395f31035c8db0e4bedf24eb4067559c
desdelgado <davidesmoleydelgado@gmail.com>
2019-01-31 04:30:52 +0900

    Added plotting of all the features

--- Analysis.py
+++ Analysis.py
@@ -6,5 +6,21 @@ Created on Thu Jan 31 02:04:58 2019
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 
-data = pd.read_csv('train_FD001.csv', sep = ' ')
+data_train1 = pd.read_csv('train_FD001.txt', sep = ' ', header = None)
+data_test1 = pd.read_csv('test_FD001.txt', sep = ' ', header = None)
+data_RUL1 = pd.read_csv('RUL_FD001.txt', sep = ' ', header = None)
+
+
+unit_1 = data_train1[data_train1[0] == 1]
+
+#%% EDA of what each sensor or setting looks like 
+plt.close('all')
+#plot all the sensor data
+plt.plot(unit_1[1], unit_1.loc[:,5:28], 'o' )
+plt.show()
+#seems like theres quite the spread
+
+#%% Scale the sensor data in a meaningful way 
